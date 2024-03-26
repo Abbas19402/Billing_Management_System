2e5f6262ad5829932979d9876ecc64c5f9b4bc97
Abbas19402 <abbas.dalal19402@gmail.com>
2024-03-26 16:57:44 +0530

    bugs resolved

--- service/user.service.js
+++ service/user.service.js
@@ -31,11 +31,8 @@ const addUser = async({ name, email, password }) => {
 const addInvoiceOrClients = async({ addType, payload }) => {
     try{
         if (addType === 'invoice') {
-            console.log(addType, payload);
             const existingClient = await Client.findOne({ email: payload.email });
             const allInvoices = await Invoice.find();
-
-            console.log(existingClient)
 
             if( !existingClient ) {
                 return {
@@ -59,14 +56,9 @@ const addInvoiceOrClients = async({ addType, payload }) => {
                 .then(res => console.log("Updated client's response: ", res))
                 .catch(err => console.log("Update's error: ", err))
             
-            console.log("New invoice to be added: ",{
-                number: allInvoices.length + 1,
-                amount: payload.amount,
-                date: payload.date,
-                clientEmail: payload.email
-            })
+        
             const newInvoice = new Invoice({
-                number: allInvoices.length + 1,
+                number: "BM"+allInvoices.length + 1,
                 amount: payload.amount,
                 date: payload.date,
                 clientEmail: payload.email
@@ -101,11 +93,10 @@ const editInvoiceOrClients = async({ editType, payload, payloadId }) => {
         if (editType === 'invoice') {
             const client = await Client.findOne({ email: payload.clientEmail });
             const existingInvoice = await Invoice.findOne({ number: payload.number });
-
             if( existingInvoice.amount != payload.amount ) {
                 await Client.updateOne(
                     { _id: client._id },
-                    { $set: {totalAmount: (client.totalAmount - existingInvoice.amount) + payload.amount} }
+                    { $set: {totalAmount: (client.totalAmount - existingInvoice.amount) + parseInt(payload.amount)} }
                 )
             }
             await Invoice.updateOne(
@@ -113,7 +104,15 @@ const editInvoiceOrClients = async({ editType, payload, payloadId }) => {
                 { $set: {amount: payload.amount} }
             );
         } else {
+            const client = await Client.findOne({ _id: payloadId });
+            if( client.email != payload.email ) {
+                await Invoice.updateOne(
+                    { clientEmail: client.email },
+                    { $set: {clientEmail: payload.email} }
+                )
+            }
             await Client.updateOne({ _id: payloadId }, payload);
+
         }
 
         return {
@@ -132,16 +131,19 @@ const deleteInvoiceOrClient = async({ deleteType, payloadId }) => {
     try {
         if( deleteType == 'invoice' ) {
             const existingInvoice = await Invoice.findOne({ _id: payloadId });
+            const client = await Client.findOne({ email: existingInvoice.clientEmail });
             await Client.updateOne(
-                { _id: existingInvoice.clientId },
-                { $inc: {totalAmount: -existingInvoice.amount} }
+                { _id: client._id },
+                { $set: {totalAmount: (client.totalAmount - existingInvoice.amount)} }
             );
             await Invoice.deleteOne({ _id: payloadId });
         } else {
-            console.log(payloadId)
+            const existingClient = await Client.findOne({ _id: payloadId })
             await Client.deleteOne({ _id: payloadId })
                 .then(deleteRes => console.log("Deleted client: ",deleteRes))
                 .catch(deleteErr => console.log("Deleted error: ",deleteErr))
+            
+            await Invoice.deleteMany({ clientEmail: existingClient.email });
         }
 
         return {
