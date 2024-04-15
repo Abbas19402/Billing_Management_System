5a6aaf065acc6b0c26bee3c38dc9a5184c030de7
Abbas Ali Dalal <abbas.dalal19402@gmail.com>
2024-04-15 19:46:15 +0530

    Update: Cors configuration

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const routes = require('./routes')
 
 const app = express();
 
-app.use(cors({
-    origin: "https://aad-billing-management.vercel.app/"
-}));
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
