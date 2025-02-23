61e7945b9999cfabeb2f72dc0b419e54cf315351
Abbas Dalal <91382081+Abbas19402@users.noreply.github.com>
2025-02-23 13:28:25 +0530

    Move server to apps/server

--- routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const authController = require('../controllers/auth.controller')
-const express = require('express');
-
-const router = express.Router();
-
-router.post('/login',authController.login);
-
-module.exports = router;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const userRoutes = require('./user.route');
-const authRoutes = require('./auth.routes');
-
-const routes = {
-    user: userRoutes,
-    auth: authRoutes
-};
-
-module.exports = routes;
